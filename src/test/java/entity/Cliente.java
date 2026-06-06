aa53b3cca303f7b1f719ec501cc7123922e9df1d
AlessandroFontana04 <alefontana2004@gmail.com>
2026-06-06 15:56:41 +0200

    Modifiche ad Utente e StoricoOrdini, Inizio visualizzaElencoOrdini

--- src/test/java/entity/Cliente.java
+++ src/test/java/entity/Cliente.java
@@ -15,6 +15,24 @@ public class Cliente {
         if(ordine == null) return false;
         if(!ordiniPersonali.contains(ordine)) return false;
         ordine.setStato(Stato.ANNULLATO);
+        return true;
+    }
+
+    public List<Ordine> visualizzaElencoOrdini() {
+
+        //idea di base: invoco l'information expert StoricoOrdini e cerco gli ordini del cliente
+        //successivamente, li passo ad ordiniPersonali, ovvero la lista visualizzabile dalla singola istanza di Cliente
+
+        //parte di transferimento
+        StoricoOrdini storico = StoricoOrdini.getInstance();
+        List<Ordine> tuttiGliOrdini= storico.getOrdini();
+        for (Ordine ordine : tuttiGliOrdini) {
+            if(ordiniPersonali.getEmail().equals(this.getEmail())){
+                ordiniPersonali.add(ordine);
+            };
+        }
+
 
     }
+
 }
