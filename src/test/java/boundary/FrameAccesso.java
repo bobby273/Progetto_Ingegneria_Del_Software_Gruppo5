bd9fdae6a42e2b133ec25a7cd34c79a4307a49ec
Giammy98 <gianmariafranco22@gmail.com>
2026-06-06 12:10:19 +0200

    v1.1

--- src/test/java/boundary/FrameAccesso.java
+++ src/test/java/boundary/FrameAccesso.java
@@ -17,24 +17,27 @@ public class FrameAccesso {
 
     public FrameAccesso(){
         accediButton.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        String email = textFieldEmail.getText();
-                        String password = passwordField.getText();
-                        int esito = ControllerAccesso.checkTipoUtente(email, password);
-                        if (esito == ControllerAccesso.UTENTE_AMM) {
-                            //chiamata alla frame di admin
-                            labelEsito.setText("Amministratore");
-                        } else if (esito == ControllerAccesso.UTENTE_CLIE){
-                            //chiamata alla frame di cliente
-                            labelEsito.setText("Cliente");
-                        }
-                        else
-                            //chiamata a frame di errore?
-                            labelEsito.setText("Utente non trovato");
-                }
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String email = textFieldEmail.getText();
+                String password = passwordField.getText();
+                int esito = -1;
+                if(email.length()<=40 && password.length()<=40 && password.length()>=8 && email.contains("@")){
+                    esito = ControllerAccesso.checkTipoUtente(email, password);
+                    if (esito == ControllerAccesso.UTENTE_AMM) {
+                        //chiamata alla frame di admin
+                        labelEsito.setText("Amministratore");
+                    } else if (esito == ControllerAccesso.UTENTE_CLIE){
+                        //chiamata alla frame di cliente
+                        labelEsito.setText("Cliente");
+                    }
+                    else
+                        //chiamata a frame di errore?
+                        labelEsito.setText("Utente non trovato");
+                } else
+                    labelEsito.setText("Errore dimensione campi.");
             }
-        );
+        });
     }
 
     public JFrame apriFormAcc(){
