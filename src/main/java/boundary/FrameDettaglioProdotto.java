fcdfb1d7ed2d6d941663bad74a50b87c86222f7f
man-for04 <fornataro.ma@gmail.com>
2026-06-07 18:53:33 +0200

    GUI migliorata

--- src/main/java/boundary/FrameDettaglioProdotto.java
+++ src/main/java/boundary/FrameDettaglioProdotto.java
@@ -6,9 +6,12 @@ import com.intellij.uiDesigner.core.Spacer;
 import controller.ControllerCliente;
 
 import javax.swing.*;
+import javax.swing.plaf.FontUIResource;
+import javax.swing.text.StyleContext;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Locale;
 
 public class FrameDettaglioProdotto extends JFrame {
 
@@ -50,27 +53,26 @@ public class FrameDettaglioProdotto extends JFrame {
         });
     }
 
-    private void fillProdotto(String nome, float prezzo, String categoria, int qtaDisp, boolean isScontato, String descrizione){
+    private void fillProdotto(String nome, float prezzo, String categoria, int qtaDisp, boolean isScontato, String descrizione) {
         lblNomeProdotto.setText(nome);
         lblPrezzoProdotto.setText(String.valueOf(prezzo) + " €");
         lblCategoriaProdotto.setText(categoria);
         lblQtaDispProdotto.setText(String.valueOf(qtaDisp));
 
-        if(isScontato){
+        if (isScontato) {
             lblScontatoProdotto.setText("Sì");
-        }
-        else{
+        } else {
             lblScontatoProdotto.setText("No");
         }
         txtAreaDescrizioneProdotto.setText(descrizione);
 
     }
 
-    private boolean checkInput(String nomeProdotto, int qtaDesiderata){
+    private boolean checkInput(String nomeProdotto, int qtaDesiderata) {
         boolean esito = false;
-        if(!nomeProdotto.isEmpty() && nomeProdotto.length() < 1000){
+        if (!nomeProdotto.isEmpty() && nomeProdotto.length() < 1000) {
 
-            if(!ControllerCliente.esisteProdotto(nomeProdotto)){
+            if (!ControllerCliente.esisteProdotto(nomeProdotto)) {
                 JOptionPane.showMessageDialog(
                         null,
                         "Errore! Non presente nel catalogo!",
@@ -83,7 +85,7 @@ public class FrameDettaglioProdotto extends JFrame {
                 //Quantità inserita valida; si procede al controller
                 esito = ControllerCliente.AggiungiAlCarrello(nomeProdotto, qtaDesiderata);
 
-                if(esito){
+                if (esito) {
                     //tutto ok, aggiorna la visualizzazione del carrello
                     lblEsito.setText("Aggiunto al carrello!");
                     spinnerQtaDesiderataProdotto.setValue(0);
@@ -96,8 +98,7 @@ public class FrameDettaglioProdotto extends JFrame {
                             "Conferma aggiunta al carrello",
                             JOptionPane.INFORMATION_MESSAGE
                     );
-                }
-                else{
+                } else {
                     JOptionPane.showMessageDialog(
                             null,
                             "Errore! Condizioni verificate, ma qualcosa è andato storto!",
@@ -120,8 +121,7 @@ public class FrameDettaglioProdotto extends JFrame {
                         JOptionPane.ERROR_MESSAGE
                 );
             }
-        }
-        else{
+        } else {
             //prodotto non presente nel catalogo
             lblEsito.setText("ERRORE! Prodotto non presente nel catalogo o non valido!");
             lblEsito.setForeground(Color.RED);
@@ -168,16 +168,18 @@ public class FrameDettaglioProdotto extends JFrame {
     private void $$$setupUI$$$() {
         panelProdotto = new JPanel();
         panelProdotto.setLayout(new GridLayoutManager(7, 3, new Insets(2, 2, 2, 2), -1, -1));
+        panelProdotto.setBackground(new Color(-657931));
         panelProdotto.setMinimumSize(new Dimension(450, 300));
         panelProdotto.setPreferredSize(new Dimension(1450, 1300));
         final JLabel label1 = new JLabel();
+        Font label1Font = this.$$$getFont$$$("Segoe UI", Font.BOLD, 18, label1.getFont());
+        if (label1Font != null) label1.setFont(label1Font);
+        label1.setForeground(new Color(-13877680));
         label1.setText("Nome: ");
         panelProdotto.add(label1, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         lblNomeProdotto = new JLabel();
         lblNomeProdotto.setText("Label");
         panelProdotto.add(lblNomeProdotto, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(116, 17), null, 0, false));
-        final Spacer spacer1 = new Spacer();
-        panelProdotto.add(spacer1, new GridConstraints(0, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
         final JLabel label2 = new JLabel();
         label2.setText("Prezzo: ");
         panelProdotto.add(label2, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
@@ -205,41 +207,65 @@ public class FrameDettaglioProdotto extends JFrame {
         lblScontatoProdotto = new JLabel();
         lblScontatoProdotto.setText("Label");
         panelProdotto.add(lblScontatoProdotto, new GridConstraints(4, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        final Spacer spacer1 = new Spacer();
+        panelProdotto.add(spacer1, new GridConstraints(1, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
         final Spacer spacer2 = new Spacer();
-        panelProdotto.add(spacer2, new GridConstraints(1, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
+        panelProdotto.add(spacer2, new GridConstraints(3, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
         final Spacer spacer3 = new Spacer();
-        panelProdotto.add(spacer3, new GridConstraints(2, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
+        panelProdotto.add(spacer3, new GridConstraints(4, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
         final Spacer spacer4 = new Spacer();
-        panelProdotto.add(spacer4, new GridConstraints(3, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
-        final Spacer spacer5 = new Spacer();
-        panelProdotto.add(spacer5, new GridConstraints(4, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
-        final Spacer spacer6 = new Spacer();
-        panelProdotto.add(spacer6, new GridConstraints(5, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
+        panelProdotto.add(spacer4, new GridConstraints(5, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
         final JPanel panel1 = new JPanel();
         panel1.setLayout(new GridLayoutManager(3, 4, new Insets(0, 0, 0, 0), -1, -1));
         panelProdotto.add(panel1, new GridConstraints(6, 0, 1, 2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
         final JLabel label7 = new JLabel();
         label7.setText("Quantità desiderata:");
         panel1.add(label7, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-        final Spacer spacer7 = new Spacer();
-        panel1.add(spacer7, new GridConstraints(1, 0, 2, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_VERTICAL, 1, GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
+        final Spacer spacer5 = new Spacer();
+        panel1.add(spacer5, new GridConstraints(1, 0, 2, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_VERTICAL, 1, GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
         spinnerQtaDesiderataProdotto = new JSpinner();
         panel1.add(spinnerQtaDesiderataProdotto, new GridConstraints(0, 1, 1, 2, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         btnAggiungiACarrello = new JButton();
         btnAggiungiACarrello.setText("Aggiungi al carrello");
         panel1.add(btnAggiungiACarrello, new GridConstraints(1, 1, 1, 2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-        final Spacer spacer8 = new Spacer();
-        panel1.add(spacer8, new GridConstraints(2, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_VERTICAL, 1, GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
-        final Spacer spacer9 = new Spacer();
-        panel1.add(spacer9, new GridConstraints(1, 3, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
+        final Spacer spacer6 = new Spacer();
+        panel1.add(spacer6, new GridConstraints(2, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_VERTICAL, 1, GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
+        final Spacer spacer7 = new Spacer();
+        panel1.add(spacer7, new GridConstraints(1, 3, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
         lblEsito = new JLabel();
         lblEsito.setText("Label");
         panel1.add(lblEsito, new GridConstraints(0, 3, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-        final Spacer spacer10 = new Spacer();
-        panelProdotto.add(spacer10, new GridConstraints(6, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
+        final Spacer spacer8 = new Spacer();
+        panelProdotto.add(spacer8, new GridConstraints(6, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
         txtAreaDescrizioneProdotto = new JTextArea();
         txtAreaDescrizioneProdotto.setEditable(false);
         panelProdotto.add(txtAreaDescrizioneProdotto, new GridConstraints(5, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_WANT_GROW, null, new Dimension(150, 50), null, 0, false));
+        final Spacer spacer9 = new Spacer();
+        panelProdotto.add(spacer9, new GridConstraints(0, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
+        final Spacer spacer10 = new Spacer();
+        panelProdotto.add(spacer10, new GridConstraints(2, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
+    }
+
+    /**
+     * @noinspection ALL
+     */
+    private Font $$$getFont$$$(String fontName, int style, int size, Font currentFont) {
+        if (currentFont == null) return null;
+        String resultName;
+        if (fontName == null) {
+            resultName = currentFont.getName();
+        } else {
+            Font testFont = new Font(fontName, Font.PLAIN, 10);
+            if (testFont.canDisplay('a') && testFont.canDisplay('1')) {
+                resultName = fontName;
+            } else {
+                resultName = currentFont.getName();
+            }
+        }
+        Font font = new Font(resultName, style >= 0 ? style : currentFont.getStyle(), size >= 0 ? size : currentFont.getSize());
+        boolean isMac = System.getProperty("os.name", "").toLowerCase(Locale.ENGLISH).startsWith("mac");
+        Font fontWithFallback = isMac ? new Font(font.getFamily(), font.getStyle(), font.getSize()) : new StyleContext().getFont(font.getFamily(), font.getStyle(), font.getSize());
+        return fontWithFallback instanceof FontUIResource ? fontWithFallback : new FontUIResource(fontWithFallback);
     }
 
     /**
@@ -249,4 +275,7 @@ public class FrameDettaglioProdotto extends JFrame {
         return panelProdotto;
     }
 
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
